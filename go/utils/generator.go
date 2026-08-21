1a9e43fe859f00c7a4dd413e3bb8f2835c2640f4
Pitchaya Worapaluk <bhoon2012@gmail.com>
2026-08-21 13:08:28 +0700

    Add(feat): InintState

--- go/utils/generator.go
+++ go/utils/generator.go
@@ -13,3 +13,13 @@ func InverseTransformWifiUser() float64 {
 	}
 	return wifi
 }
+
+func InitState(ET0 float64, ET1 float64) float64 {
+	u := rand.Float64()
+	P0 := ET0 / (ET0 + ET1)
+
+	if u <= P0 {
+		return 0
+	}
+	return 1
+}
